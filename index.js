a77041962f5fef31dbfc4b515ea9a44496e55d3f
Ubuntu <ubuntu@ip-172-31-10-87.us-west-1.compute.internal>
2017-06-21 06:05:34 +0000

    fixup! Implement the google map example

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT_NUMBER = 8080
+const PORT_NUMBER = 8080;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/mymap.html'));
-})
+});
 
 app.listen(PORT_NUMBER, function () {
-  console.log('Example app listening on port ' + PORT_NUMBER + '!')
-})
-
+  console.log('Example app listening on port %s!', PORT_NUMBER);
+});
