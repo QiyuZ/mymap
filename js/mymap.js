12f171ff252bc266fde5cb28cabc5cab91729548
Ubuntu <ubuntu@ip-172-31-10-87.us-west-1.compute.internal>
2017-06-23 04:02:52 +0000

    Do nearby search on the fronend

--- js/mymap.js
+++ js/mymap.js
@@ -1,11 +1,35 @@
+var DEFAULT_ZOOM = 15;
+var GOOGLE_API_KEY = 'AIzaSyDFDg1ruZ4GQqPHMv1Z4Lprn_2wXyd0NYk';
+
+
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var position = {
+    lat: 37.773972,
+    lng: -122.43129
+  };
+
   var map = new google.maps.Map($('#map')[0], {
-    zoom: 4,
-    center: uluru
+    zoom: DEFAULT_ZOOM,
+    center: position
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: position,
     map: map
+  });
+
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+    data: {
+      'location': position.lat + ',' + position.lng,
+      'type': 'restaurant',
+      'key': GOOGLE_API_KEY,
+      'radius': 500
+    },
+    success: function(data) {
+      debugger;
+    },
+    failure: function(data) {
+      debugger;
+    }
   });
 }
