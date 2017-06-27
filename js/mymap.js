13ac10310815f6a016ea7ca2d33728e918077960
Ubuntu <ubuntu@ip-172-31-10-87.us-west-1.compute.internal>
2017-06-27 06:36:07 +0000

    Show detailed place info

--- js/mymap.js
+++ js/mymap.js
@@ -49,9 +49,23 @@ $(function() {
             }
             infowindow.open(map, marker);
             current_infowindow = infowindow;
+            showDetailedInfo(place);
           });
         });
       }
+    });
+  }
+
+  function showDetailedInfo(place) {
+    var params = {
+      placeId: place['place_id']
+    };
+    service.getDetails(params, function(place) {
+      $('#hero-header-wrapper img').attr('src', place.photos[0].getUrl({'maxWidth': 408, 'maxheight': 407}));
+      $('.place-name').text(place['name']);
+      $('.place-review-score').text(place['rating']);
+      $('.place-type').text(place['types'][0]);
+      $('#place-info-wrapper').show();
     });
   }
 
