3daf4e9ea34b7bf9f75bfacc9bdd2fb0f9848a46
Ubuntu <ubuntu@ip-172-31-10-87.us-west-1.compute.internal>
2017-06-27 04:52:12 +0000

    Show google maps

--- js/mymap.js
+++ js/mymap.js
@@ -1,35 +1,56 @@
 var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyDFDg1ruZ4GQqPHMv1Z4Lprn_2wXyd0NYk';
+var DEFAULT_RADIUS = 1500;
+var DEFAULT_LAT = 37.773972;
+var DEFAULT_LNG = -122.43129;
 
 $(function() {
   function initMap() {
-    var position = {
-      lat: 37.773972,
-      lng: -122.43129
-    };
-
     var map = new google.maps.Map($('#map')[0], {
       zoom: DEFAULT_ZOOM,
-      center: position
-    });
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map
+      center: {
+        lat: DEFAULT_LAT,
+        lng: DEFAULT_LNG
+      }
     });
 
-    $.ajax({
-      url: '/nearby_search',
-      data: {
-        'location': position.lat + ',' + position.lng,
-        'type': 'restaurant',
-        'key': GOOGLE_API_KEY,
-        'radius': 500
-      },
-      success: function(data) {
-        debugger;
-      },
-      failure: function(data) {
-        debugger;
+    var params = {
+      'location': new google.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+      'radius': DEFAULT_RADIUS,
+      'type': 'restaurant'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(params, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var current_infowindow;
+
+        _.each(results, function(place) {
+          var marker = new google.maps.Marker({
+            position: {
+              'lat': place.geometry.location.lat(),
+              'lng': place.geometry.location.lng()
+            },
+            map: map
+          });
+
+          var infowindow_content =
+            '<div id="content">' +
+              '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>'+
+            '</div>';
+
+          var infowindow = new google.maps.InfoWindow({
+            content: infowindow_content
+          });
+
+          marker.addListener('click', function() {
+            if (current_infowindow) {
+              current_infowindow.close();
+            }
+            infowindow.open(map, marker);
+            current_infowindow = infowindow;
+          });
+        });
       }
     });
   }
